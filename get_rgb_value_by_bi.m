6cd3cf331b71a581ee29dd0e8ab3c8736299735e
Parv Parkhiya <parvparkhiya@live.com>
2018-03-10 19:02:59 +0530

    computation is still too costly

--- get_rgb_value_by_bi.m
+++ get_rgb_value_by_bi.m
@@ -2,7 +2,7 @@ function rgbf = get_rgb_value_by_bi(I, px, py)
 	x = [floor(px), ceil(px)];
 	y = [floor(py), ceil(py)];
 
-	I = double(I);
+	% I = double(I);
 
 	rgbx1 = I(x(1), y(1), :) * (1 - (px - floor(px))) + I(x(2), y(1), :) * (px - floor(px));
 
--- main.m
+++ main.m
@@ -1,7 +1,11 @@
 % take image input Ig and Ii
-Ig=imread('SourceImage/gate1_guide.jpg');
-Ii=imread('SourceImage/gate1_input.jpg');
+Ig=double(imread('SourceImage/gate1_guide.jpg'))/255;
+Ii=double(imread('SourceImage/gate1_input.jpg'))/255;
 
+resize_factor=0.1;
+
+Ig=imresize(Ig,resize_factor);
+Ii=imresize(Ii,resize_factor);
 %
 % 265,392 Top left
 % 1341,392 Top right
@@ -12,10 +16,10 @@ Ii=imread('SourceImage/gate1_input.jpg');
 % take bounding box for rough estimate
 % input format [xmin ymin width height]
 
-Bgi = [265,392,1075,714];
+Bgi = ceil([265,392,1075,714]*resize_factor);
 
 % create Igi image from above
-Igi=imread('SourceImage/gate1_combi.jpg');
+Igi = imcrop(Ig,Bgi);
 
 % create discretized values for trans, rot, scaling (
 % may consider forming a struct for transformation instead)
@@ -24,7 +28,7 @@ rot = linspace(-45, 45, 11);
 refl = [-1, 1];
 
 % initialize datastructure to store the best value for each branch
-best_maps = []
+% best_maps = []
 
 % initialize branch index to keep track of branch_num (for indexing of final maps)
 b_idx = 0;
@@ -33,11 +37,16 @@ b_idx = 0;
 for s=1:size(scal,2)
 	for r=1:size(rot,2)
 		for re=1:size(refl,2)
-			b_idx =b_idx+1 1;
+			b_idx =b_idx+1;
 			% do something
-			best_maps[b_idx] = process_branch(Ig, Igi, Ii, scal(s), rot(r), refl(re));
+			% best_maps[b_idx] = process_branch(Ig, Igi, Ii, scal(s), rot(r), refl(re));
+            [best_translation,maxTxTy,num_patches,contributor_histogram]=process_branch(Ig, Igi,Bgi, scal(s), rot(r), refl(re));
+			
+			break;
 		end
+		break;
 	end
+	break;
 end
 
 % perform alpha expansion to get photomontage
