c91b815c8b49c1d240e473b44378a4f2be25828e
akankshabaranwal <akankshabar@gmail.com>
2018-03-10 17:11:45 +0530

    main updated

--- main.m
+++ main.m
@@ -1,13 +1,21 @@
 % take image input Ig and Ii
-Ig = imread();
-Ii = imread();
+Ig=imread('SourceImage/gate1_guide.jpg');
+Ii=imread('SourceImage/gate1_input.jpg');
 
+%
+% 265,392 Top left
+% 1341,392 Top right
+% 266,1106 Bottom left
+% 1341,1106 Bottom right
+% width=1075
+% height=714
 % take bounding box for rough estimate
 % input format [xmin ymin width height]
-Bgi = [];
+
+Bgi = [265,392,1075,714];
 
 % create Igi image from above
-Igi = Imcrop(Ig, Bgi);
+Igi=imread('SourceImage/gate1_combi.jpg');
 
 % create discretized values for trans, rot, scaling (
 % may consider forming a struct for transformation instead)
@@ -22,21 +30,21 @@ best_maps = []
 b_idx = 0;
 
 % process each branch
-for s=scal
-	for r=rot
-		for re=refl
-			b_idx += 1;
+for s=1:size(scal,2)
+	for r=1:size(rot,2)
+		for re=1:size(refl,2)
+			b_idx =b_idx+1 1;
 			% do something
-			best_maps[b_idx] = process_branch(Ig, Igi, Ii, scal, rot, refl);
+			best_maps[b_idx] = process_branch(Ig, Igi, Ii, scal(s), rot(r), refl(re));
 		end
 	end
 end
 
 % perform alpha expansion to get photomontage
-transform_guide_map = get_photo_montage();
+%transform_guide_map = get_photo_montage();
 
 % Apply transformations to get final image from Ii
-I = get_image_from_transform_map(transform_guide_map, Ii);
+%I = get_image_from_transform_map(transform_guide_map, Ii);
 
-imshow(I);
-imwrite(I, 'generated_image.png');
+%imshow(I);
+%imwrite(I, 'generated_image.png');
