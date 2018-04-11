680dce4baf482ce6b236f27870baf2b2740a7756
dreamer_iiit <parvparkhiya@live.com>
2018-04-11 17:47:06 +0530

    Smooth cost added in final alpha

---
+++ gco-v3.0/parv_build/CMakeFiles/alpha_exp_final.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/alpha_exp_final.dir/alpha_exp_final.cpp.o"
+  "alpha_exp_final.pdb"
+  "alpha_exp_final"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/alpha_exp_final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
